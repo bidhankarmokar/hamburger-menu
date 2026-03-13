c9018f252469e8ae44c144c363c77066d9132a8e
Bidhan Karmokar <bidhankarmokar45@gmail.com>
2026-03-13 22:07:59 +0600

    Second commit

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
 const hamburgerMenu = document.querySelector(".hamburger");
 const navBar = document.querySelector(".nav");
 
-hamburgerMenu.addEventListener("click", ()=> { 
-    navBar.classList.toggle("active");
-    hamburgerMenu.classList.toggle("active");
-})
+hamburgerMenu.addEventListener("click", () => {
+  navBar.classList.toggle("active");
+  hamburgerMenu.classList.toggle("active");
+});
 
 // Nav bar hide/show on scroll
 
 const body = document.body;
 let lastScroll = 0;
 
-window.addEventListener("scroll", ()=> { 
-    const currentScroll = window.pageYOffset;
-    if(currentScroll <= 0){ 
-        body.classList.add("scroll-up");
-        return;
-    }
-    if(currentScroll < lastScroll && body.classList.contains("scroll-down")){ 
-        body.classList.add("scroll-up");
-        body.classList.remove("scroll-down");
-    }
-    else if(currentScroll > lastScroll && !body.classList.contains("scroll-down")){ 
-        body.classList.remove("scroll-up");
-        body.classList.add("scroll-down");
-    }
-    lastScroll = currentScroll;
-})
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+
+  navBar.classList.remove("active");
+  hamburgerMenu.classList.remove("active");
+  if (currentScroll <= 0) {
+    body.classList.add("scroll-up");
+    return;
+  }
+  if (currentScroll < lastScroll && body.classList.contains("scroll-down")) {
+    body.classList.add("scroll-up");
+    body.classList.remove("scroll-down");
+  } else if (
+    currentScroll > lastScroll &&
+    !body.classList.contains("scroll-down")
+  ) {
+    body.classList.remove("scroll-up");
+    body.classList.add("scroll-down");
+  }
+  lastScroll = currentScroll;
+});
