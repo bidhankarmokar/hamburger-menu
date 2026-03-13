6273814153a2238d39524454602b74ba19afe68c
Bidhan Karmokar <bidhankarmokar45@gmail.com>
2026-03-13 21:48:45 +0600

    First commit

--- script.js
+++ script.js
@@ -5,3 +5,25 @@ hamburgerMenu.addEventListener("click", ()=> {
     navBar.classList.toggle("active");
     hamburgerMenu.classList.toggle("active");
 })
+
+// Nav bar hide/show on scroll
+
+const body = document.body;
+let lastScroll = 0;
+
+window.addEventListener("scroll", ()=> { 
+    const currentScroll = window.pageYOffset;
+    if(currentScroll <= 0){ 
+        body.classList.add("scroll-up");
+        return;
+    }
+    if(currentScroll < lastScroll && body.classList.contains("scroll-down")){ 
+        body.classList.add("scroll-up");
+        body.classList.remove("scroll-down");
+    }
+    else if(currentScroll > lastScroll && !body.classList.contains("scroll-down")){ 
+        body.classList.remove("scroll-up");
+        body.classList.add("scroll-down");
+    }
+    lastScroll = currentScroll;
+})
